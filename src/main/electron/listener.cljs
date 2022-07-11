a905370b33b466475a79de210343d0198df81133
Junyi Du <junyidu.cn@gmail.com>
2022-07-11 16:28:14 +0800

    enhance: make x-callback-url/getCurrentURL more semantic

--- src/main/electron/listener.cljs
+++ src/main/electron/listener.cljs
@@ -8,7 +8,12 @@
             [electron.ipc :as ipc]
             [frontend.config :as config]
             [frontend.context.i18n :refer [t]]
-            [frontend.date :as date]
+            [frontend.handler.route :as route-handler]
+            [frontend.handler.editor :as editor-handler]
+            [frontend.handler.ui :as ui-handler]
+            [cljs-bean.core :as bean]
+            [frontend.fs.watcher-handler :as watcher-handler]
+            [frontend.fs.sync :as sync]
             [frontend.db :as db]
             [frontend.db.model :as db-model]
             [frontend.fs.sync :as sync]
@@ -18,7 +23,6 @@
             [datascript.core :as d]
             [electron.ipc :as ipc]
             [frontend.ui :as ui]
-            [frontend.util.url :as url-util]
             [frontend.handler.notification :as notification]
             [frontend.handler.page :as page-handler]
             [frontend.handler.repo :as repo-handler]
@@ -26,8 +30,9 @@
             [frontend.handler.ui :as ui-handler]
             [frontend.handler.user :as user]
             [frontend.state :as state]
-            [frontend.ui :as ui]))
-
+            [frontend.ui :as ui]
+            [frontend.handler.url-protocol :as url-protocol-handler]
+            [frontend.handler.user :as user]))
 
 (defn persist-dbs!
   []
@@ -160,73 +165,13 @@
 
   (js/window.apis.on "quickCapture"
                      (fn [args]
-                       (let [{:keys [url title content page append]} (bean/->clj args)
-                             insert-today? (get-in (state/get-config)
-                                                   [:quick-capture-options :insert-today?]
-                                                   false)
-                             redirect-page? (get-in (state/get-config)
-                                                    [:quick-capture-options :redirect-page?]
-                                                    false)
-                             today-page (when (state/enable-journals?)
-                                          (string/lower-case (date/today)))
-                             page (if (or (= page "TODAY")
-                                          (and (string/blank? page) insert-today?))
-                                    today-page
-                                    (or (not-empty page)
-                                        (state/get-current-page)
-                                        today-page))
-                             page (or page "quick capture") ;; default to quick capture page, if journals are not enabled
-                             format (db/get-page-format page)
-                             time (date/get-current-time)
-                             text (or (and content (not-empty (string/trim content))) "")
-                             link (if (string/includes? url "www.youtube.com/watch")
-                                    (str title " {{video " url "}}")
-                                    (if (not-empty title)
-                                      (config/link-format format title url)
-                                      url))
-                             template (get-in (state/get-config)
-                                              [:quick-capture-templates :text]
-                                              "**{time}** [[quick capture]]: {text} {url}")
-                             content (-> template
-                                         (string/replace "{time}" time)
-                                         (string/replace "{url}" link)
-                                         (string/replace "{text}" text))
-                             edit-content (state/get-edit-content)
-                             edit-content-blank? (string/blank? edit-content)
-                             edit-content-include-capture? (and (not-empty edit-content)
-                                                                (string/includes? edit-content "[[quick capture]]"))]
-                         (if (and (state/editing?) (not append) (not edit-content-include-capture?))
-                           (if edit-content-blank?
-                             (editor-handler/insert content)
-                             (editor-handler/insert (str "\n" content)))
-
-                           (do
-                             (editor-handler/escape-editing)
-                             (when (not= page (state/get-current-page))
-                               (page-handler/create! page {:redirect? redirect-page?}))
-                             ;; Or else this will clear the newly inserted content
-                             (js/setTimeout #(editor-handler/api-insert-new-block! content {:page page
-                                                                                            :edit-block? true
-                                                                                            :replace-empty-target? true})
-                                            100))))))
+                       (let [{:keys [url title content page append]} (bean/->clj args)]
+                         (url-protocol-handler/quick-capture url title content page append))))
 
   (js/window.apis.on "handleGetCurrentURL"
                      (fn [args]
-                       (let [{:keys  [x-success x-error?]} (bean/->clj args)
-                             repo (state/get-current-repo)
-                             ;; may reuse the following logic for other api services
-                             edit-block-id   (some-> (state/get-edit-block) (:block/uuid))
-                             select-block-id (first (state/get-selection-block-ids))
-                             cur-page-id     (some-> (state/get-current-page) (db-model/get-page) (:block/uuid))
-                             uuid            (or edit-block-id select-block-id cur-page-id)
-
-                             url     (when uuid
-                                       (url-util/encode-param (url-util/get-logseq-graph-uuid-url nil repo uuid)))
-                             target  (if (and x-error? (nil? url))
-                                       x-error?
-                                       (str x-success url))]
-                         (js/setTimeout #(js/window.open target) 100) ;; Magic, don't remove the timeout, even 1ms works. Why?
-                         )))
+                       (let [{:keys  [x-success x-error?]} (bean/->clj args)]
+                         (url-protocol-handler/get-current-url x-success x-error?))))
 
   (js/window.apis.on "openNewWindowOfGraph"
                      ;; Handle open new window in renderer, until the destination graph doesn't rely on setting local storage
